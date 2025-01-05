8546ab82406aac27d4daf8f85f859ec9aac31a39
Nicholas Casqueira <nickcas37@gmail.com>
2025-01-05 17:45:58 -0500

    Add project files.

---
+++ Vector2D.cpp
@@ -0,0 +1,45 @@
+#include "Vector2D.h"
+
+double Vector2D::getMagnitude() {
+	return (sqrt(x * x + y * y));
+}
+Vector2D Vector2D::normalize() {
+	// vector / magnitude of vector
+	double magnitude = getMagnitude();
+	return Vector2D(x / magnitude, y / magnitude);
+}
+
+Vector2D Vector2D::operator+(const Vector2D& other) const {
+	return Vector2D(x + other.x, y + other.y);
+}
+Vector2D Vector2D::operator-(const Vector2D& other) const {
+	return Vector2D(x - other.x, y - other.y);
+}
+Vector2D Vector2D::operator/(const Vector2D& other) const {
+	return Vector2D(x / other.x, y / other.y);
+}
+Vector2D Vector2D::operator*(const Vector2D& other) const {
+	return Vector2D(x * other.x, y * other.y);
+}
+
+
+Vector2D Vector2D::operator+(const double& amount) const {
+	return Vector2D(x + amount, y + amount);
+}
+Vector2D Vector2D::operator-(const double& amount) const {
+	return Vector2D(x - amount, y - amount);
+}
+Vector2D Vector2D::operator*(const double& amount) const {
+	return Vector2D(x * amount, y * amount);
+}
+Vector2D Vector2D::operator/(const double& amount) const {
+	return Vector2D(x / amount, y / amount);
+}
+
+
+bool Vector2D::operator=(const Vector2D& other) const {
+	return (x == other.x && y == other.y);
+}
+bool Vector2D::operator!=(const Vector2D& other) const {
+	return (x != other.x && y != other.y);
+}
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "SDL.h"
+#include "Game.h"
+#include "Vector2D.h"
+
+int main(int argc, char* argv[])
+{
+	// Variables
+	std::string windowTitle = "SDL Window";
+	int windowWidth = 800;
+	int windowHeight = 600;
+
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "Error: ";
+		std::cout << SDL_GetError();
+		SDL_Quit();
+	}
+	else {
+		window = SDL_CreateWindow(windowTitle.c_str(),
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, 0);
+		
+		if (window == nullptr) {
+			std::cout << SDL_GetError() << std::endl;
+		}
+		else {
+			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+			if (renderer == nullptr) {
+				std::cout << SDL_GetError() << std::endl;
+			}
+			else {
+				// For transparent graphics
+				SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+				// Anti-Aliasing
+				SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+
+				// render Driver
+				SDL_RendererInfo rendererInfo;
+				SDL_GetRendererInfo(renderer, &rendererInfo);
+				std::cout << "Renderer = " << rendererInfo.name << std::endl;
+
+				// Start Game
+				Game game(window, renderer, windowWidth, windowHeight);
+
+				// Destroy Renderer 
+				SDL_DestroyRenderer(renderer);
+			}
+		}
+		SDL_DestroyWindow(window);
+	}
+	SDL_Quit();
+
+	return 0;
+}
